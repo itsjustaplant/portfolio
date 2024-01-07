edc4a57e29998051d83811a16b841f97ff0d4196
itsjustaplant <alperensahin442@gmail.com>
2024-01-07 12:35:12 +0000

    feat :: implement blog pages

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # my website
 built with astro
 thx astro
+
+# todo
+[ ] - merge tag and blog buttons into one component as button/link for reusability
+[ ] - move spotify api
+[ ] - create letterboxd api
+[ ] - dockerize
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: true,
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'dracula' },
+    gfm: false
+  })],
+});

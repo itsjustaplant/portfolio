d7451a2c8597ed2406df70732c44324eec719689
itsjustaplant <alperensahin442@gmail.com>
2024-01-11 14:21:52 +0000

    performance :: enable text compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
@@ -11,5 +13,5 @@ export default defineConfig({
       theme: 'dracula'
     },
     gfm: false
-  }), react()]
+  }), react(), compressor()]
 });
